7da5e0e636bdb005c41f7a3fe0372f571e5f48d5
AntonioSkopin <59810086+AntonioSkopin@users.noreply.github.com>
2020-04-18 12:52:25 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
-<php
-  
-     /*
-     *  Author: Antonio Skopin
-     *  Date: 17th of april 2020
-     */
-     
+<?php
+
+/*
+ * Author: Antonio Skopin
+ * Date: 18th april
+ */
+
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Rock Paper Scissors</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous">
     <style>
         body{
             background-color: silver;
@@ -18,57 +25,74 @@
         }
 
         .box{
-            margin: 2rem auto 0;
-            padding: 2rem;
-            background-color: #f3f3f3;
-            height: 500px;
             width: 440px;
+            height: 500px;
+            padding: 2rem;
+            margin: 2rem auto 0;
             border-radius: 10px;
+            background-color: #f3f3f3;
         }
 
         form{
             display: flex;
             margin-top: 5rem;
+            padding-bottom: 5rem;
         }
-
     </style>
 </head>
 <body>
+    <!--  Container for the content  -->
     <div class="box">
         <h1>Rock Paper Scissors</h1>
-        <form method="get" action="index.php" autocomplete="off">
-            <input type="text" name="keuze" placeholder="Make your choice" class="form-control">
-            <input type="submit" name="submit" value="Shoot!" class="btn btn-success mb-2">
+        <form action="index.php" method="post" autocomplete="off">
+            <input type="text" name="choice" class="form-control">
+            <input type="submit" name="shoot" value="Shoot" class="btn btn-success" />
         </form>
         <?php
-            if(isset($_GET['submit'])){
-                $keuze = $_GET['keuze'];
-                $compkeuze = rand(0, 2);
+            // Checks if shoot button is pressed
+            if (isset($_POST['shoot'])){
+                $choice = $_POST['choice'];
+                // Computers choice will be a random integer for now we will assign that later
+                $compChoice = rand(0, 2);
 
-                if($keuze == ""){
-                    echo "<div class=\"alert alert-danger\" role=\"alert\">Please enter a valid choice.</div>";
-                }else if ($keuze == "rock" || $keuze == "paper" || $keuze == "scissors"){
-                    echo "<div class=\"alert alert-dark\" role=\"alert\">Your choice is $keuze.</div>";
-                    switch ($compkeuze){
+                // Checks if input is empty
+                if($choice == ""){
+                    echo "<div class=\"alert alert-danger\" role=\"alert\">Please enter a valid choice!</div>";
+                }else if ($choice == "rock" || $choice == "paper" || $choice == "scissors"){
+                    // Checks if input is equal to either rock, paper or scissors
+                    // Outputs your choice in an alert
+                    echo "<div class=\"alert alert-dark\" role=\"alert\">You chose $choice!</div>";
+
+                    // Switch case for the choice of the computer and assign it to a word
+                    // If the random number is 0 it means rock
+                    // If the random number is 1 it means paper
+                    // If the random number is 2 it means scissors
+                    switch ($compChoice){
                         case 0:
-                            echo "<div class=\"alert alert-warning\" role=\"alert\">The computer chooses rock.</div>";
+                            echo "<div class=\"alert alert-warning\" role=\"alert\">The computer chose rock!</div>";
                             break;
                         case 1:
-                            echo "<div class=\"alert alert-warning\" role=\"alert\">The computer chooses paper.</div>";
+                            echo "<div class=\"alert alert-warning\" role=\"alert\">The computer chose paper!</div>";
                             break;
                         case 2:
-                            echo "<div class=\"alert alert-warning\" role=\"alert\">The computer chooses scissors.</div>";
+                            echo "<div class=\"alert alert-warning\" role=\"alert\">The computer chose scissors!</div>";
                             break;
+                        // No need for default case the variable won't generate a number larger than 2
                     }
-                    if (($keuze == "rock" && $compkeuze == 0) || ($keuze == "paper" && $compkeuze == 1) || ($keuze == "scissors" && $compkeuze == 2)){
+
+                    // Checks if the game is a tie
+                    if(($choice == "rock" && $compChoice == 0) || ($choice == "paper" && $compChoice == 1) || ($choice == "scissors" && $compChoice == 3)){
                         echo "<div class=\"alert alert-info\" role=\"alert\">You chose the same as the computer it's a tie!</div>";
-                    }else if(($keuze == "rock" && $compkeuze == 2) || ($keuze == "paper" && $compkeuze == 0) || ($keuze == "scissors" && $compkeuze == 1)){
+                    }else if(($choice == "rock" && $compChoice == 2) || ($choice == "paper" && $compChoice == 0) || ($choice == "scissors" && $compChoice == 1)){
+                        // Code if the user wins
                         echo "<div class=\"alert alert-success\" role=\"alert\">You have beaten the computer!</div>";
-                    }else {
+                    }else{
+                        // Code if the computer wins
                         echo "<div class=\"alert alert-danger\" role=\"alert\">The computer beats you!</div>";
                     }
                 }else{
-                    echo "<div class=\"alert alert-danger\" role=\"alert\">Please enter a valid choice.</div>";
+                    // This alert will popup when it isn't a valid choice or isn't empty
+                    echo "<div class=\"alert alert-danger\" role=\"alert\">Please enter a valid choice!</div>";
                 }
             }
         ?>
